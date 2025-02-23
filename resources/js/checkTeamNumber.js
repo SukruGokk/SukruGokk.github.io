cfc5fa54a33b22fa4a18bf17c44cdd434d18191f
Şükrü Erdem Gök <62439670+SukruGokk@users.noreply.github.com>
2025-02-24 00:01:19 +0300

    Update checkTeamNumber.js

--- resources/js/checkTeamNumber.js
+++ resources/js/checkTeamNumber.js
@@ -2,24 +2,18 @@ import { getFirestore, getDocs, getDoc, setDoc, collection, doc, deleteDoc } fro
 import { db, teamsPage } from '/resources/js/index.js';
 
 export async function checkTeamNumber(teamNumber){
-    let urls = ['https://bluealliance-proxy.onrender.com/proxy/event/2025tuis/teams/simple', 'https://bluealliance-proxy.onrender.com/proxy/event/2025tuhc/teams/simple']
     let team_name = false;
 
-    for (const url of urls) {
-        const response = await fetch(url, {
-            headers: { 'X-TBA-Auth-Key': 'ruNe33FUWNuhNaxvC9yvh11uOepI4Q6RUwpHkuUWPRrWLWwWRHULmwtTyC7jpgAt' }
-        });
-        const json = await response.json();
-        let teams = Object.values(json);
-        
-        for (const team of teams) {
-            if (team.team_number == teamNumber) {
-                team_name = team.nickname;
-                break;
-            }
+    const response = await fetch('/resources/teams.json');
+    const json = await response.json();
+    let teams = json.teams;
+    console.log(teams);
+    
+    for (const team of teams) {
+        if (team.team_number == teamNumber) {
+            team_name = team.nickname;
+            break;
         }
-
-        if (team_name) break;
     }
     return team_name;
 }
